5b6ee61d4b223b433c8339859811a152c07d8647
DaveFloresSN <93693403+DaveFloresSN@users.noreply.github.com>
2022-05-05 15:34:17 +0800

    Added Item Controller

---
+++ Assets/Scripts/Interactions/ItemController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public int maxItems = 2;
+    public Items[] heldItems;
+
+
+}
