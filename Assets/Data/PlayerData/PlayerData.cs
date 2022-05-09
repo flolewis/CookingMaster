a20d91816cc980ec32960533291db50a56281c39
DaveFloresSN <93693403+DaveFloresSN@users.noreply.github.com>
2022-05-09 15:46:58 +0800

    Added Player Data

---
+++ Assets/Data/PlayerData/PlayerData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Player Data",menuName = "Data/Player")]
+public class PlayerData : ScriptableObject
+{
+    public float timer;
+    public float score;
+}
